404c8b3a77bf583336eff5e8edf986bade2c82dc
L0c_d0G <94112336+ruslanbadaev01@users.noreply.github.com>
2023-01-31 19:03:13 +0300

    .map() Render Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,6 +4,8 @@ function Categories() {
 
   const [activeCategories, setCategories] = useState(0)
 
+  const categories = ['Все','Мясные','Вегетарианская','Гриль','Острые','Закрытые']
+const idCategories = [0,1,2,3,4,5]
   const onclickCategories = (index)=>{
     setCategories(index)
   }
@@ -11,12 +13,11 @@ function Categories() {
     return (
       <div className="categories">
         <ul>
-          <li onClick={()=> {onclickCategories(0)}} className={activeCategories == 0 ? "active" : ""}>Все</li>
-          <li onClick={()=> {onclickCategories(1)}} className={activeCategories == 1 ? "active" : ""}>Мясные</li>
-          <li onClick={()=> {onclickCategories(2)}} className={activeCategories == 2 ? "active" : ""}>Вегетарианская</li>
-          <li onClick={()=> {onclickCategories(3)}} className={activeCategories == 3 ? "active" : ""}>Гриль</li>
-          <li onClick={()=> {onclickCategories(4)}} className={activeCategories == 4 ? "active" : ""}>Острые</li>
-          <li onClick={()=> {onclickCategories(5)}} className={activeCategories == 5 ? "active" : ""}>Закрытые</li>
+          {
+            categories.map((pizaName,pizzaIndex)=> (
+              <li onClick={()=> onclickCategories(pizzaIndex)} className={activeCategories == pizzaIndex ? "active" : ""}>{pizaName}</li>
+            ))
+          }
         </ul>
       </div>
     );
